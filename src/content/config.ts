8ee337af72bfa7f2aec681974e07b5bfb3e7f936
sandroid <maxwell.edison@gmail.com>
2023-11-27 19:11:10 +0000

    attempt to sort blog entries by date

--- src/content/config.ts
+++ src/content/config.ts
@@ -11,12 +11,14 @@ const blogCollection = defineCollection({
     schema: z.object({
         title: z.string(),
         tags: z.array(TagsEnum),
+        date: z.string(),
     })
  });
 const photoCollection = defineCollection({ 
   schema:  z.object({
     title: z.string(),
     tags: z.array(TagsEnum),
+    date: z.string(),
 }),
 
  });
