8b81db716cd632feb8efbc81d4862ff0169bb7c1
lorempipsum <maxwell.edison69@gmail.com>
2025-06-03 19:32:38 +0100

    Fix: Update react-spring imports to use @react-spring/web in Title and Logo components

--- src/components/react/Title/Logo.tsx
+++ src/components/react/Title/Logo.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useRef } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated } from '@react-spring/web';
 import styles from './Logo.module.scss';
 import SanderArrow from './SanderArrow';
 
--- src/components/react/Title/Title.tsx
+++ src/components/react/Title/Title.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useSpring, animated } from 'react-spring';
+import { useSpring, animated } from '@react-spring/web';
 import Logo from './Logo';
 import styles from './Title.module.scss';
 
